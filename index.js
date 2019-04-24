3cbe009d4dc4efc4cccdb3269af7d5fc0a73eef4
caiogustavo15 <caiogustavo15@hotmail.com>
2019-04-24 21:27:13 +0100

    chages to info display

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require ('path');
+const http = require('http');
+const https = require('https');
 
 const app = express();
 
 // //static folder
 app.use(express.static(path.join(__dirname, 'i2project')));
-//
+////
+app.get('/README.md', function(req, res) {
+  res.sendFile(path.join(__dirname,'./README.md'));
+});
+////
 const PORT = process.env.PORT || 5000 ;
 //
 app.listen (PORT, () => console.log(`started server port ${PORT}`));
