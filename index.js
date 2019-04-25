cd6cf12cf4d9c0d8c5647a7ea823d6c7ebe239f2
caiogustavo15 <caiogustavo15@hotmail.com>
2019-04-25 19:33:58 +0100

    new README.md

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require ('path');
 const http = require('http');
 const https = require('https');
+const markdown = require ('markdown').markdown;
+var fs = require('fs');
+
+let tet = fs.readFileSync('README.md','utf8')
+let tt = markdown.toHTML(tet);
 
 const app = express();
 
@@ -9,7 +14,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'i2project')));
 ////
 app.get('/README.md', function(req, res) {
-  res.sendFile(path.join(__dirname,'./README.md'));
+  // res.sendFile(path.join(__dirname,'./README.md'));
+  res.send(tt);
 });
 ////
 const PORT = process.env.PORT || 5000 ;
